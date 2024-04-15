227ee4485addf05b9c39d2cff8a0529dfed8fcb3
Raman Hafiyatulin <r.gafiyatullin@me.com>
2024-04-15 22:49:51 +0300

    build using docker into the specified volume

---
+++ .dev/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+BUILDER_IMAGE=${BUILDER_IMAGE:-'build-reth:dev'}
+TARGET_DIR_VOLUME=${TARGET_DIR_VOLUME:-'ops-bedrock_reth_build'}
+DOCKER=${DOCKER:-docker}
+
+
+THIS_ZSH="$0:A"
+THIS_BASH="$BASH_SOURCE"
+THIS_DIR=$(cd "$(dirname ${BASH_SOURCE:-"$THIS_ZSH"})"; pwd)
+PROJECTS_DIR=$(dirname $(dirname "$THIS_DIR"))
+
+docker build -t "${BUILDER_IMAGE}" "${THIS_DIR}"
+
+cd "${PROJECTS_DIR}/reth"
+
+
+
+in-docker \
+    --image "${BUILDER_IMAGE}" \
+    --name build-reth \
+    -v "${THIS_DIR}/cargo-git:/usr/local/cargo/git:rw" \
+    -v "${THIS_DIR}/cargo-registry:/usr/local/cargo/registry:rw" \
+    -v "${TARGET_DIR_VOLUME}:${PROJECTS_DIR}/reth/target:rw" \
+    -v "${PROJECTS_DIR}/alloy:${PROJECTS_DIR}/alloy:ro"
