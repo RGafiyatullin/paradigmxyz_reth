da43bcc352d875af35184984b5fbe5adbfbdb6fa
Raman Hafiyatulin <r.gafiyatullin@me.com>
2024-04-21 01:00:28 +0300

    UnknownSafeOrFinalizedBlock error should contain "block not found" so that op-node could recognize it

--- crates/rpc/rpc/src/eth/error.rs
+++ crates/rpc/rpc/src/eth/error.rs
@@ -43,7 +43,7 @@ pub enum EthApiError {
     UnknownBlockNumber,
     /// Thrown when querying for `finalized` or `safe` block before the merge transition is
     /// finalized, <https://github.com/ethereum/execution-apis/blob/6d17705a875e52c26826124c2a8a15ed542aeca2/src/schemas/block.yaml#L109>
-    #[error("unknown block")]
+    #[error("unknown block (block not found, right?)")]
     UnknownSafeOrFinalizedBlock,
     /// Thrown when an unknown block or transaction index is encountered
     #[error("unknown block or tx index")]
